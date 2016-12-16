cff0c05afe27692bcc0741c6b2473bafcc0de538
twodawg <mtwohey2@yahoo.com>
2016-12-15 22:37:04 -0800

    Example C#

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MasterDealer01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("------ Ranks --------");
+            for (int i = 0; i < 13; i++)
+            {
+                //Console.WriteLine(i);
+                Console.WriteLine((RankType)i);
+            }
+            Console.WriteLine("------ Suits --------");
+            for (int i = 0; i < Enum.GetNames(typeof(SuitType)).Length; i++)
+            {
+                Console.WriteLine((SuitType)i);
+            }
+            Console.ReadLine();
+        }
+    }
+    public enum SuitType
+    {
+        Spades,
+        Clubs,
+        Diamonds,
+        Hearts
+    }
+    public enum RankType
+    {
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace
+    }
+}
